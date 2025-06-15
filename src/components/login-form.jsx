077af2cbef8a4facc12ec75d48b89fdaf1083492
Ayushman Manishankar <ayushmanmanishankar@Ayushmans-MacBook-Air.local>
2025-06-16 00:59:37 +0530

    build issue solved

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { createSupabaseBrowserClient } from "@/lib/supabase/browser-client"
+import { useState } from "react"
 
 export function LoginForm({
   className,
