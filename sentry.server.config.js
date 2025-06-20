158ea52388a22c830d4353b5e579074c3a585516
Ayushman Manishankar <ayushmanmanishankar@Ayushmans-MacBook-Air.local>
2025-06-20 23:13:27 +0530

    added sentry analytics and speed insights

---
+++ sentry.server.config.js
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://db7a58efe872c1042cd0bf6a9c25194a@o4509532172189696.ingest.us.sentry.io/4509532173172736",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
